ef80da2467c11eef4095c66d9b01c2dfd600a642
daylightcoderj6 <jananibharathi6@gmail.com>
2025-03-28 07:34:51 +0530

    created layout for audits page

--- src/Pages/Audits.jsx
+++ src/Pages/Audits.jsx
@@ -3,7 +3,29 @@ import { Link } from 'react-router-dom'
 
 const Audits = () => {
   return (
-       <div >audits</div>
+       <div >
+        <div className='bg-soft-secondary m-8 rounded-xl'>
+          <div>grpah</div>
+          <div>graph value details</div>
+        </div>
+       
+       {/* <div className='flex flex-row justify-evenly'> */}
+       <div className='grid grid-cols-3 gap-4'>
+          <div  className='bg-soft-secondary m-8 rounded-md col-span-2'>total audits</div>
+          <div  className='bg-soft-secondary m-8 rounded-md'>internal audits</div>
+        </div>
+        <div className='grid grid-cols-3 gap-4'>
+          <div  className='bg-soft-secondary m-8 rounded-md'>financial audits</div>
+          <div  className='bg-soft-secondary m-8 rounded-md'>external audits</div>
+          <div  className='bg-soft-secondary m-8 rounded-md'>compliance audits</div>
+        </div>
+        <div className='grid grid-cols-3 gap-4'>
+          <div  className='bg-soft-secondary m-8 rounded-md'>IT audits</div>
+          <div  className='bg-soft-secondary m-8 rounded-md'>Forensic audits</div>
+          <div  className='bg-soft-secondary m-8 rounded-md'>Performance audits</div>
+        </div>
+       </div>
+      //  </div>
   
   )
 }
